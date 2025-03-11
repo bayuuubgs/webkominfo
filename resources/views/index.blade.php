6c80f4e81efe4bcffa7e56796e6e7f3c1e6e2784
bayuuubgs <armedas1578@gmail.com>
2025-03-11 14:20:08 +0700

    ketiga

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,16 @@
 <?php
-echo "sajnxasnamxsanxjansjn"
+echo "sajnxasnamxsanxjansjndfghjgfcdxfcgvbhjnkhgyvbhnjkhugyfvbhnjkhujgyvbhnjkihujgyvbnhjkih"
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello word</h1>
+</body>
+</html>
