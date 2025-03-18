4ad28238a010dbbc57b6942a8e9cfe465808cfab
Hilmi Arya Rafwa Muhammad <hilmi.arya2006@gmail.com>
2025-03-18 14:45:58 +0700

    tailwind.config.js 18/03/2025

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        jakarta: ["'Plus Jakarta Sans'", "sans-serif"],
+      },
+      fontSize: {
+        display: ["64px", { lineHeight: "64px" }],
+        h1: ["40px", { lineHeight: "55px" }],
+        h2: ["24px", { lineHeight: "31px" }],
+        h3: ["18px", { lineHeight: "27px" }],
+        p: ["14px", { lineHeight: "21px" }],
+        label: ["10px", { lineHeight: "10px" }],
+      },
+      colors: {
+        gray90: "#181818",
+        gray70: "#484848",
+        gray50: "#919191",
+        gray30: "#F2F2F2",
+        gray10: "#FFFFFF",
+        primary100: "#1e1b4b",
+        primary50: "#4338ca",
+        primary30: "#6366f1",
+        secondary70: "#E63946",
+        secondary50: "#FF7B82",
+        background: "#E9EEF4",
+        backgroundred: "#F7ECEC"
+      },
+    },
+  },
+  plugins: [],
+}
