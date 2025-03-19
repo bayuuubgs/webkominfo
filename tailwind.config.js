ffa0faa31992beff7be97e5e5f8e3754156a9ce5
bayuuubgs <armedas1578@gmail.com>
2025-03-19 13:16:44 +0700

    Resolve merge conflict in index.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                jakarta: ["'Plus Jakarta Sans'", "sans-serif"],
-            },
-            fontSize: {
-                display: ["64px", { lineHeight: "64px" }],
-                h1: ["40px", { lineHeight: "55px" }],
-                h2: ["24px", { lineHeight: "31px" }],
-                h3: ["18px", { lineHeight: "27px" }],
-                p: ["14px", { lineHeight: "21px" }],
-                label: ["10px", { lineHeight: "10px" }],
-            },
-            colors: {
-                gray90: "#181818",
-                gray70: "#484848",
-                gray50: "#919191",
-                gray30: "#F2F2F2",
-                gray10: "#FFFFFF",
-                primary100: "#1F185F",
-                primary50: "#3B2CB5",
-                primary30: "#7A74F0",
-                secondary70: "#E63946",
-                secondary50: "#FF7B82",
-                background: "#E9EEF4",
-                backgroundred: "#F7ECEC",
-            },
-        },
-    },
-};
-=======
 module.exports = {
   content: [
     "./resources/**/*.blade.php",
@@ -74,4 +35,3 @@ module.exports = {
   },
   plugins: [],
 }
->>>>>>> 4ad28238a010dbbc57b6942a8e9cfe465808cfab
