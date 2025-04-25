8f1321216d88fe9b95dee5e0e4aa6f78a4321ea6
Hilmi Arya Rafwa Muhammad <hilmi.arya2006@gmail.com>
2025-04-25 11:00:55 +0700

    profil + detail galeri

--- resources/views/components/card-kategori-unitkerja.blade.php
+++ resources/views/components/card-kategori-unitkerja.blade.php
@@ -2,21 +2,29 @@
     'titleFirst' => 'Rekomendasi',
     'titleSecond' => 'Kategori',
     'items' => [],
+    'bgColor' => 'bg-background',
+    'titleFirstColor' => 'text-gray90',
+    'titleSecondColor' => 'text-primary50',
+    'titleSize' => 'text-h2',
+    'gap' => 'gap-4', // default gap antar card
+    'titleMarginBottom' => 'mb-8', // default margin bawah title
 ])
 
-<div class="bg-background py-10">
-    <div class="flex justify-center mb-8">
+<div class="{{ $bgColor }} py-10">
+    <div class="flex justify-center {{ $titleMarginBottom }}">
         <div class="flex gap-2 items-center text-center">
-            <h2 class="text-gray90 font-bold text-[24px]">{{ $titleFirst }}</h2>
-            <h2 class="text-primary50 font-bold text-[24px]">{{ $titleSecond }}</h2>
+            <h2 class="{{ $titleFirstColor }} font-bold {{ $titleSize }}">{{ $titleFirst }}</h2>
+            <h2 class="{{ $titleSecondColor }} font-bold {{ $titleSize }}">{{ $titleSecond }}</h2>
         </div>
     </div>
 
-    <div class="flex justify-center">
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full max-w-screen-xl">
+    <div class="flex justify-center items-center">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 {{ $gap }} w-full max-w-screen-xl">
             @foreach ($items as $item)
-                <div class="relative w-[266px] h-[306px] bg-white rounded-[16px] overflow-hidden shadow-md group hover:ring-2 hover:ring-blue-500 transition-all duration-300">
+                <div class="relative w-[266px] h-[306px] bg-white rounded-xl overflow-hidden shadow-xl group hover:shadow-2xl hover:ring-2 hover:ring-primary30 transition-all duration-300">
                     <img src="{{ $item['image'] }}" alt="Card Image" class="w-full h-full object-cover">
+
+                    <div class="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-gray90 to-transparent"></div>
                     
                     @if(isset($item['text']))
                         <div class="absolute bottom-0 left-0 right-0 text-white text-center py-6">
