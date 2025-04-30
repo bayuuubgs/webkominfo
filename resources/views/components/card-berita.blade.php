f40c5e6083c61d79ad73b88af966c082f69ff655
Hilmi Arya Rafwa Muhammad <hilmi.arya2006@gmail.com>
2025-04-30 11:23:41 +0700

    rabu pagi

--- resources/views/components/card-berita.blade.php
+++ resources/views/components/card-berita.blade.php
@@ -1,19 +1,25 @@
 <div>
     <a href="{{ $link }}" class="relative block {{ $cardWidth }} h-auto font-jakarta bg-transparent rounded-2xl overflow-hidden transition-all group hover:shadow-lg">
         <div
-            class="transition duration-200 ease-out absolute inset-0 rounded-2xl border-2 border-transparent {{ $hoverBorder }} group-hover:p-4">
+            class="transition duration-200 ease-out absolute inset-0 rounded-2xl border border-transparent {{ $hoverBorder }} group-hover:p-2">
         </div>
 
         <div class="relative z-10 bg-transparent p-4 rounded-2xl">
-            <img src="{{ $image }}" alt="Card Image" class="w-full {{ $imageHeight }} object-cover shadow-sm rounded-lg">
-            <h3 class="text-h3 font-bold {{ $textColor }} mt-2 whitespace-normal line-clamp-2">{{ $title }}</h3>
+            <img src="{{ $image }}" alt="Card Image" class="w-full aspect-[1.5] object-cover shadow-lg rounded-lg">
+            
+            <h3 class="text-h3 font-bold {{ $textColor }} mt-2 whitespace-normal line-clamp-2">
+                {{ $title }}
+            </h3>
+
             <div class="flex justify-between items-center mt-2">
                 <p class="text-p {{ $dateColor }} font-light">{{ $date }}</p>
                 <x-label-kategori text="{{ $category }}" />
             </div>
+
             <p class="{{ $textColor }} mt-2 text-p text-justify font-light line-clamp-3 whitespace-normal">
                 {{ $description }}
             </p>
+
             <span class="text-blue-400 font-semibold text-p">
                 {{ $readMoreText ?? 'Baca Selengkapnya' }}
             </span>
--- resources/views/components/card-galeri.blade.php
+++ resources/views/components/card-galeri.blade.php
@@ -1,4 +1,4 @@
 <div class="relative overflow-hidden shadow group">
-    <img src="{{ $image }}" alt="Galeri" class="w-full h-[250px] object-cover transition duration-500 group-hover:brightness-110">
+    <img src="{{ $image }}" alt="Galeri" class="w-full h-[250px] object-scale-down transition duration-500 group-hover:brightness-110">
     <div class="absolute inset-0 bg-gradient-to-b from-gray50/25 via-transparent to-primary100/50 pointer-events-none z-10 transition duration-500 group-hover:opacity-80"></div>
 </div>
