7cb653e75a82da67406043b3b80273e02c382e92
rakhanaufallllll <rakhanaufal1504@gmail.com>
2025-05-07 12:23:16 +0700

    pull responsive

--- resources/views/components/card-lainnya.blade.php
+++ resources/views/components/card-lainnya.blade.php
@@ -1,12 +1,14 @@
 <!-- @props(['title' => 'Video', 'highlight' => 'Lainnya']) -->
 
-<div class="w-full max-w-[474px] bg-gray10 rounded-2xl shadow-md p-2 mx-auto">
-    <h2 class="text-h2M md:text-h2 font-semibold mb-2 text-center font-responsive">
-        <span class="text-gray90">{{ $title }}</span>
-        <span class="text-primary50">{{ $highlight }}</span>
-    </h2>
+<div class="block md:flex md:justify-start">
+    <div class="w-full max-w-[474px] bg-gray10 rounded-2xl shadow-md p-2 mx-auto md:mx-0">
+        <h2 class="text-h2M md:text-h2 font-semibold mb-2 text-center font-responsive">
+            <span class="text-gray90">{{ $title }}</span>
+            <span class="text-primary50">{{ $highlight }}</span>
+        </h2>
 
-    <div class="space-y-2.5">
-        {{ $slot }}
+        <div class="space-y-2.5">
+            {{ $slot }}
+        </div>
     </div>
 </div>
